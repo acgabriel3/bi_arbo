c0012d25f31697ca4151cdd3d66649de9a556aa9
acgabriel <gabriel_alvesozorio@hotmail.com>
2019-04-15 10:55:39 -0300

    Modificada função verificaNas.R para retornar os resultados de cada coluna em formato de vetor

--- CRISPDM/verificaNas.R
+++ CRISPDM/verificaNas.R
@@ -3,11 +3,36 @@
 
 verificaNas <- function(tabela){
   
+  resultado <- c(NULL)
+  
   for(i in 1:length(tabela)) {
     
+    '%UNE%' <- function(x,y) paste(x, y) 
+    
     print(paste("Quantidade Nas coluna : ", colnames(tabela)[i], " " ,sep = ""))
-    print(sum(is.na(tabela[[i]])))
+    
+    nomeVariavel <- colnames(tabela)[i]
+    nas <- sum(is.na(tabela[i]), na.rm = TRUE)
+    
+    resultado <- 
+      
+      eval(parse(text =
+                   "c(resultado," %UNE%
+                   nomeVariavel %UNE%
+                   "=" %UNE%
+                   "nas" %UNE%
+                   ")"
+        
+        
+      ))
+    
+    #***
+    #Como fazer isso sem metaprog? 
+    
+    print(nas)
     
   }
+  
+  return(resultado)
   
 }
